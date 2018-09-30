9b903442c059b7aeabcb996ece1b7e465236fb31
Daniel Fernandes <danielsf97@gmail.com>
2018-09-30 01:25:26 +0100

    cenas

--- pw.py
+++ pw.py
@@ -15,7 +15,12 @@ def main():
     #    words.append(word.strip())
     print(find_prefix(0,"Boro",elements))
 
-    print(split_word("BONO", elements))
+    (ws, ws_count) = split_word("Gafetio", elements)
+    #if ws != []:
+    #	i = 0
+    #	while i < ws_count:
+    #		ws[i] = elements[ws[i]]
+    print(ws)
 
 def split_word(word, elements):
 	w_it = 0
@@ -24,15 +29,20 @@ def split_word(word, elements):
 	ws_it = 0
 	ws_size = 0
 	while(w_it < word_size):
-		o = find_prefix(0, word, elements)
+		o = find_prefix(0, word[w_it:], elements)
+		print("%%%%%")
+		print(w_it)
+		#print(elements[o])
 		if o >= 118 and ws_it > 0:
+			print(1)
 			back = 1
 			while ws_it > 0 and back == 1:
-			    w_it -= len(elements[ws[ws_it]])
-			    ws_it -= 1 
-			    o = find_prefix(ws[ws_it] + 1, word, elements)
+			    ws_it -= 1
+			    w_it -= len(elements[ws[ws_it]]) 
+			    o = find_prefix(ws[ws_it] + 1, word[w_it:], elements)
+			    ws.pop()
 			    if o != 118:
-			    	ws[ws_it] = o
+			    	ws.append(o)
 			    	w_it += len(elements[o])
 			    	ws_it += 1
 			    	back = 0
@@ -40,9 +50,12 @@ def split_word(word, elements):
 				ws = []
 				break
 		elif o >= 118 and ws_it <= 0:
+			print(2)
 			ws = []
 			break
 		else:
+			print(3)
+			ws.append(o)
 			w_it += len(elements[o])
 			ws_it += 1
 	return (ws, ws_it)
@@ -57,8 +70,7 @@ def find_prefix(i, word, elements):
 	size = len(elements)
 	found = 0
 	while i < size and not found :
-		if re.match(elements[i], word): 
-			#print(elements[i])
+		if re.match(elements[i], word, re.IGNORECASE): 
 			found = 1
 		else: i+=1
 	return i
